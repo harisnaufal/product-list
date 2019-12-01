9083f72680a4cf063efd02f333f9c03d96ddd070
harisnaufal <harisn.work@gmail.com>
2019-12-01 22:27:08 +0700

    add google fb api

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,6 @@
 import React from "react";
+import FacebookLogin from 'react-facebook-login';
+import GoogleLogin from "react-google-login";
 import { Store } from "../store/Store";
 import Actions from "../actions/Actions";
 
@@ -10,6 +12,19 @@ const Login = () => {
       payload: true
     });
   };
+  const responseFacebook = (response) => {
+    console.log('responseFacebook', response);
+  }
+  const responseGoogle = (response) => {
+    if (response.tokenId){
+      return dispatch({
+        type: Actions.LOGIN,
+        payload: true
+      });
+    } else {
+      alert('Login Failed');
+    }
+  }
   return (
     <div className="login">
       <h3 className="title">LOGIN</h3>
@@ -34,12 +49,23 @@ const Login = () => {
         </button>
       </div>
       <div className="account">
-        <button type="button" className="facebook">
+        {/* <button type="button" className="facebook">
           Sign In With Facebook
         </button>
         <button type="button" className="google">
           Sign In With Google
-        </button>
+        </button> */}
+        <FacebookLogin
+          appId="466718857310317" //APP ID NOT CREATED YET
+          fields="name,email,picture"
+          callback={responseFacebook}
+        />
+        <GoogleLogin
+          clientId="820562297374-9cb29n9osnhti8fjv8bikkl02k8sd67i.apps.googleusercontent.com" //CLIENTID NOT CREATED YET
+          buttonText="LOGIN WITH GOOGLE"
+          onSuccess={responseGoogle}
+          onFailure={responseGoogle}
+        />
       </div>
     </div>
   );
